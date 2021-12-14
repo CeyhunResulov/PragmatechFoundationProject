e8c2df91af90574e155fc1e13b801ae2c3c81cfd
Ceyhun Resulov <ceyhunresulov23@gmail.com>
2021-12-15 03:50:49 +0400

    arrey ile bagli tapsiriqlar

---
+++ ProjectFrontEnd/js-task/arr.js
@@ -0,0 +1,18 @@
+
+
+// let num = 519470714
+
+// let str = String(num)
+// let eksstr=''
+// for(let i=str.length-1;i>=0;i--){
+//     eksstr=eksstr+str[i]
+// }
+// console.log(eksstr)
+
+// let num=20002304
+// let str=String(num)
+// let arr=[]
+// for(let i=0; i<str.length; i--){
+//     arr[i]=str[i]
+// }
+// console.log(arr)
