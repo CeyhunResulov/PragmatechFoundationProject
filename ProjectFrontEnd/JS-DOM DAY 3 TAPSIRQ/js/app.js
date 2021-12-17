466251ed63d4d0512cdf4caac20563c23504ff68
Ceyhun Resulov <ceyhunresulov23@gmail.com>
2021-12-17 19:15:50 +0400

    js dom tapsiriq gallery

---
+++ ProjectFrontEnd/JS-DOM DAY 3 TAPSIRQ/js/app.js
@@ -0,0 +1,36 @@
+// 1.ugursuz
+// function changeImage() {
+//     document.querySelector(".big-image img").setAttribute("src", "images/images (2).jpg")
+//     document.querySelector(".big-image img").setAttribute("src", "images/images.jpg")
+//     document.querySelector(".big-image img").setAttribute("src", "images/indir.jpg")
+//     document.querySelector(".big-image img").setAttribute("src", "images/indir (1).jpg")
+//     document.querySelector(".big-image img").setAttribute("src", "images/indir (2).jpg")
+// }
+
+
+// 2.ugurlu
+function changeImage() {
+    document.querySelector(".big-image img").setAttribute("src", "images/images (2).jpg")
+    
+}
+function changeImage2(){
+    document.querySelector(".big-image img").setAttribute("src", "images/images.jpg")
+
+}
+function changeImage3() {
+    document.querySelector(".big-image img").setAttribute("src", "images/indir.jpg")
+
+}
+ function changeImage4() {
+     document.querySelector(".big-image img").setAttribute("src", "images/indir (1).jpg")
+
+}
+ function changeImage5() {
+     document.querySelector(".big-image img").setAttribute("src", "images/indir (2).jpg")
+
+} 
+
+
+
+
+ 
