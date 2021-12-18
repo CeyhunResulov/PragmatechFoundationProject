f4cfd53c46227fcba9d410999865dd27828b6abb
Ceyhun Resulov <ceyhunresulov23@gmail.com>
2021-12-19 02:55:38 +0400

    js dom day 4 tapsiriqi etmeye basladim

---
+++ ProjectFrontEnd/JS-DOM TAPSIRQLAR/JS DOM DAY 4/js/app.js
@@ -0,0 +1,12 @@
+
+ function backcolor(){
+    document.querySelector("body").style.background = "red"
+
+}
+function headcolor(){
+    document.querySelector(".card h1").style.color="blue"
+}
+
+function textcolor(){
+    document.querySelector("p").style.color = "orange" + document.querySelector("ul").style.color = "orange"
+}
