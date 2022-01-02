f3434e6737809611e408c6281c0d6eb4b7e7ef03
Ceyhun Resulov <ceyhunresulov23@gmail.com>
2022-01-02 23:04:28 +0400

    ededin tersine cevrilmesi

---
+++ ProjectFrontEnd/js/js functıon/app.js
@@ -0,0 +1,19 @@
+// 1. Ədədi tərsinə çevirən JavaScript funksiyasını yazın.
+// let eded = "12345"
+// let terseded=""
+// for (let i = 4; i >= 0; i--){
+
+//     terseded+= eded[i]
+    
+// }
+// console.log(terseded)
+
+
+// // 2 ci usul
+// let eded=12345
+// let str=String(eded)
+// let strarr=[]
+// for (let i=0; i<str.length;i++)
+// strarr[i]=str[i]
+// terseded=Number(strarr)
+// console.log(strarr.reverse())
