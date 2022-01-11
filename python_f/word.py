4e2689db760c5c0fc3acb85b1213b0f057a5ca45
Ceyhun Resulov <ceyhunresulov23@gmail.com>
2022-01-11 20:12:26 +0400

    string methodlarin tetbiqi

---
+++ python_f/word.py
@@ -0,0 +1,39 @@
+# 2. word = 'Python'. Verilen stringin uzerinde azı 5 dənə string method tətbiq edin
+word='python'
+#1)  capitaliz() ile cumlenin ilk herfi boyuk olur
+
+# x=word.capitalize()
+# print(x)
+
+
+# 2)center metodu yazdigimiz eded qeder simvol boslugu yaradir ve metni hemin bosluga gore merkezlesdirir
+
+# x=word.center(90)
+# print(x)
+
+
+#3) count metodu () daxilinde yazdigimiz simvollarin sayini tapir
+
+# x=word.count('ceyhun')
+# print(x)
+
+
+# 4) endwith metnin hansi simvolla bitdiyini true ve ya false ile yoxlayir
+
+# x=word.endswith('n')
+# print(x)
+
+
+
+# 5) find() metodu daxil etiyimiz simvolun indexsini tapir
+
+
+# x=word.find('y')
+# print(x)
+
+
+
+
+# 6) isalpha() metodu sring ancaq herflerden ibaretdirse true qaytarir eks halda false 
+# x=word.isalpha()
+# print(x)
