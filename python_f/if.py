35c4fcb24d5f3fd8e85fe42e70e38dc085ba67ca
Ceyhun Resulov <ceyhunresulov23@gmail.com>
2022-01-14 12:36:42 +0400

    tapsiriqlar

--- python_f/if.py
+++ python_f/if.py
@@ -72,12 +72,127 @@
 
 
 
-# a=input()
-# b=input()
-# # c=pow(a,b)
-# x=input(f'numbers:{a} {b}')
-# y=input(f'netice: {a} ustu {b} {pow(a,b)}-dir')
+# x=input('numbers:')
+# y=x.split()
+# z=int(y[0]) ** int(y[1])
+# print(input(f'netice: {y[0]} ustu {y[1]} {z}-dir'))
 
 
-# print(x)
-# print(y)
+
+
+# 9. word = “istədiyiniz sözü yaza bilərsiz” məsələn word = ‘culture’ 
+# “Nineteet Eighty-Four does not present "art-as-culture" but "art-as-function"cüməsini bir dəyişkənə mənimsədin və
+#  həmin dəyişkəndə saxlayın və word-ə verdiyiniz sözün bu dəyişkəndə olub-olmamasını yoxlayın.
+#  Əgər söz varsa, ekranda belə bir nəticə çıxarın; “Culture” sözü mətndə var. Əgər yoxdursa, “Not found” çapa verin.
+
+
+
+# txt="""Nineteet Eighty-Four does not present "art-as-culture" but "art-as-function"""
+# word=input('soz daxil et: ')
+
+
+# if word in txt:
+#     print(f"bu metmde {word} sozu var")
+
+# else:
+#     print('NOT FOUND')
+
+
+
+# 10. 65 ədədinin 22 ədədinə olan qalığı ilə nisbətinin hasilini çapa verin.
+
+
+# a=65
+# b=22
+# c=(a%b)*(int(a/b))
+# print(c)
+
+
+
+
+
+# 11. x-ə istənilən bir ədəd mənimsədin, sonra isə şərt verərək yoxlayın. X 10-dan böyükdürsə və cüt ədədirsə,
+#  ekrana “OKAY” yazılsın, əgər yuxarıdakı iki şərtdən biri ödənirsə “NOT OKAY” yazılsın, heç bir şərt ödənməzsə, “BAD” yazılsın
+
+
+# x=7
+# if x>10 and x%2==0:
+#     print("OKAY")
+# elif x>10 or x%2==0:
+#     print('NOT OKAY')
+
+# else:
+#     print('BAD')   
+     
+
+
+
+
+
+
+#  12. iki ədəd götürüb dəyişkənlərə mənimsədin.
+#  Əgər ədələrin fərqi bir-birlərinə olan nisbətin tam hissəsindən böyükdürsə, ekrana “Greater”, 
+# bərabərdirsə, “EQUAL” 
+# yox əgər kiçikdirsə, “SMALLER” yazılsın.
+
+
+
+# a=12
+# b=8
+# ferq=a-b
+# nisbet=int(a/b)
+# if ferq > nisbet:
+
+#  print('GREATER')
+
+# elif ferq==nisbet:
+#     print('EQUAL')
+
+# elif ferq<nisbet:
+#     print('SMALLER')
+
+
+
+
+
+
+# 13. String data tipi yaradın və dəyərini 5.567-yə bərabər edin. Sonra bu dəyişkənin dəyərin 10- luqlara qədər yuvarlaqlaşdırın.
+
+
+
+# a='5.567'
+
+# b=float(a)
+
+# print(b)
+
+
+
+
+
+
+# 17. x = “5.89” stringinin tam hissəsinin kubunu tapın.
+# x=5.89
+# y=int(float(x))
+# kub=pow(y,3)
+# print(kub)
+
+
+
+
+# yeni tapsiriqlar
+
+
+# 1.
+
+
+
+
+
+# 2. Dörd ədəd daxil edin. Onlardan ən böyüyünü çapa verin.
+
+
+a=5
+b=8
+c=10
+d=15
