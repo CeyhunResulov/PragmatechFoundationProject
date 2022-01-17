7ee1bcf15921af741ebeb6388fc3b5d46a3169cd
Ceyhun Resulov <ceyhunresulov23@gmail.com>
2022-01-17 20:59:50 +0400

    14 yanvar tapsiriqlarin bir hissesini yazdim

---
+++ python_f/task_14yanvar.py
@@ -0,0 +1,56 @@
+# 1.  Siyahının tək elementlərini seçmək üçün Python proqramı yazın.
+
+# def odd(my_list):
+#     tek=[]
+#     for i in my_list:
+#         if i%2!=0:
+#             tek.append(i)
+#     print(tek)
+# odd([1, 3 , 8, 28, 39,107])
+
+
+
+
+# 2. Siyahıdakı bütün ededleri cəmləmək üçün Python funksiyasını yazın.
+
+# def sum(my_list):
+#     cem=0
+#     for i in my_list:
+#         cem+=i
+#     print(cem)
+# sum([4, 2, 5, 7])
+
+
+
+
+# 3. Sample List : (8, 2, 3, 0, 7) Expected Output : 20
+
+# def sum(my_list):
+#     cem=0
+#     for i in my_list:
+#         cem+=i
+#     print('netice:',cem)
+# sum((8, 2, 3, 0, 7))
+
+
+
+
+# 4. Siyahıdakı bütün ədədləri vurmaq üçün Python funksiyasını yazın.
+
+# def vurma(my_list):
+#     vur=1
+#     for i in my_list:
+#         vur=vur*i
+#     print(vur)
+#vurma((1, 3, 4, 8, 10))
+
+
+
+
+
+# 5. returnDay adlı funksiyanı yazın. Bu funksiya bir parametr götürür (1-7 arası rəqəm) və
+#  həftənin gününü qaytarır (1 bazar, 2 bazar ertəsi və s.). Rəqəm 1-dən kiçik və ya 7-dən böyükdürsə, funksiya Yox qaytarmalıdır.
+
+
+
+
