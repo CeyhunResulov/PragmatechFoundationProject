fac7409545981c2e63ac97f5939a33d8daac7fc6
Ceyhun Resulov <ceyhunresulov23@gmail.com>
2022-01-20 21:02:11 +0400

    task 18 yanvar bir hissesi

---
+++ python_f/task_18yan.py
@@ -0,0 +1,39 @@
+# 1. Üç müxtəlif a, b, c ədədləri verilmişdir. Onlardan qiymətcə orta olanı tapın.
+# Giriş verilənləri
+# a, b, c ədədləri mütləq qiymətcə 1000-i aşmayan tam ədədlərdir.
+
+
+# def edediorta(my_list):
+#     orta=(my_list[0]+my_list[1]+my_list[2])/len(my_list)
+#     print(orta)
+# edediorta([int(input('firstnum:')),int(input('secondnum:')),int(input('thirdnum:'))])
+
+
+
+
+
+# 2. Tort
+# Proqramlaşdırma olimpiadasının ikinci turu bitdikdən sonra olimpiada iştirakçıları bu hadisəni qeyd etmək qərarına gəldilər.
+#  Bu məqsədlə düzbucaqlı formada bir böyük tort sifarış verildi. Bu zaman iştirakçıların ətrafına toplaşdıqları masa dairəvi idi.
+#  Təbii ki, onlarda sual yarandı: Düzbucaqlı tort masadan qırağa çıxmamaq şərti ilə masaya yerləşəcəkdirmi? Tortun ölçülərini və 
+# masanın radiusunu bilərək, bu sualı cavablandırmalısınız.
+
+# Giriş verilənləri
+# Üç natural ədəd: masanın r (1 ≤ r ≤ 1000) radiusu, tortun w eni və l uzunluğu (1 ≤ w ≤ l ≤ 1000).
+
+# Çıxış verilənləri
+# Tort masaya yerləşirsə YES, əkshalda NO çap edin.
+
+# Giriş verilənləri masanin radiusu=38 tortun eni=40 uzunlugu=60;    Çıxış verilənləri; YES
+# Giriş verilənləri 35 20 70;    Çıxış verilənləri;NO
+
+# r=35
+# e=20
+# u=70
+# if r*2>u:
+#     print('YES') 
+# else:
+#     print('NO')
+
+
+
