4099bb03ddb9b8ea9bf474bed0379d12e57db639
Ceyhun Resulov <ceyhunresulov23@gmail.com>
2022-02-06 03:16:33 +0400

    part 2 task 10 tam bitirmemisem

--- Alqorithms/form_task.py
+++ Alqorithms/form_task.py
@@ -1,44 +1,103 @@
 
-
-class person():
-    def __init__(self,_emr,_name,_password,_sys,_list):
-        self.name=_name
-        self.passw=_password
-        self.emr=_emr
-        self.into_sys=_sys
-        self.user_list=_list
-
-    def data_base(self):
-        self.user_list.append(self.name) 
-        return self.user_list  
-    def get_name(self):
-        if self.emr==1:
-            return self.name
+from cgi import test
+from re import S
+from cryptography.fernet import Fernet
 
 
-    def password(self):
-        if self.emr==1:
-            return self.passw
+
+while True:
+    print('qeydiyyatdan kecmek ucun [1] yazin\n   -istifadeci adiniz:\n   -password:\nsisteme daxil olmaq ucun [2] yazin:\nana menuya qayitmaq ucun [3] yazin:\nsistemden cixmaq ucun [4] yazin:')
+    class person():
+        def __init__(self,_emr,_name,passw,list):
+            self.emr=_emr
+            self.name=_name
+            self.passw=passw
+            self.list=list
+    
+        def get_name(self):
+            if self.emr==1:
+                return self.name
+
+        def password(self):
+            if self.emr==1:
+                return self.passw 
 
 
-    def yoxla(self):
-        if self.name in self.user_list:
-            print(f'{self.name} siz daha once qeydiyyatdan kecmisiz')
-        else:
-            print(f'{self.name} siz ilk defedirki qeydiyyatdan kecirsiz')
+        def enc(self):
+            encpass = self.passw
+            key = Fernet.generate_key()
+            fernet = Fernet(key)
+            encpass = fernet.encrypt(encpass.encode())
+            return encpass
 
-    def get_sys(self):
-        if self.into_sys==2:
-            print(f'{self.name} xos gelmisiz.Siz artiq sisteme daxil olmusuz.')
+        def data_base(self):
+            self.list.append(self.name) 
+            return self.list  
+    
+        def check_user_list(self):
+            if self.name in self.list:
+                print(f'{self.name} siz daha once qeydiyyatdan kecmisiz')
+            else:
+                print(f'{self.name} siz ilk defedirki qeydiyyatdan kecirsiz')
+
+    user_name=person(int(input()),input('-istifadeci adiniz:'),input('parolunuz:'),[])
+    user_name.get_name()
+    user_name.check_user_list()
+    user_name.data_base()
+    user_name.password()
+    user_name.enc()
+
+
+    class sys(person):
+        def __init__(self,_sys):
+            self.system=_sys
             
-
-user_name=person(int(input('qeydiyyatdan kecmek ucun [1] yazin:')),input('-istifadeci adiniz:'),input('parolunuz:'),int(input('sisteme daxil olmaq ucun [2] yazin:')),[])
-
-user_name.get_name()
-user_name.data_base()
-user_name.yoxla()
-user_name.password()
-user_name.get_sys()
-
+        def syst(self):
+            self.system=2
+            if self.system==2:
+                print('xos gelmisiz.siz sisteme daxil olmusuz')               
+               
+    system=sys(int(input('sisteme daxil olmaq ucun [2] yazin:')))
+    system.syst()
 
 
+    class Menu(sys):
+        def __init__(self, menu):
+            self.menu=menu
+
+
+        def menu_in(self):
+            global m
+            self.menu==3
+            m=self.menu
+            return m
+
+
+
+
+    anamenu=Menu(int(input('ana menu:')))
+    anamenu.menu_in()
+    if m!=3:
+        break
+    
+
+
+
+    class out(Menu):
+        def __init__(self,outsys):
+            self.out_sys=outsys
+
+
+        def out_system(self):
+            global s 
+            self.out_sys=4
+            s=self.out_sys
+            return s
+
+    sys_=out(int(input('sistemden cix:')))
+
+    sys_.out_system()
+
+
+    if s==4:
+        break
