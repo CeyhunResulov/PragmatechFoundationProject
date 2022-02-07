d619f6c6462e7cf0efee7f85897e1273832eb10c
Ceyhun Resulov <ceyhunresulov23@gmail.com>
2022-02-07 15:38:29 +0400

    contries adli faylda olkeler adli dicti import ederek tapsiriqlari etdim

---
+++ Alqorithms/form3_task2.py
@@ -0,0 +1,71 @@
+from countries import olkeler
+
+
+
+
+
+# def FindCity(cityname):
+#     for i in olkeler.keys():
+#         x = olkeler.get(i)     
+#         if cityname in x:
+#             print(i)
+    
+# FindCity(input())
+
+#   # seher adi daxil edildiyi zaman o seherin aid olduğu ölkəni göstərsin
+  
+
+
+
+
+
+
+# def FindCountry(countryname):
+#     for i in olkeler.keys(): 
+#         if i == countryname:
+#             print(olkeler.get(i))
+# FindCountry(input())  
+#   # olke adi daxil edildiyi zaman o olkeye aid olan seherlerin adlarini ekranda gostersin
+  
+
+
+
+
+
+# def CityCountMax():
+#     count_list = []
+    
+#     for i in olkeler.keys(): 
+#         x = olkeler.get(i)
+#         count_city = int(len(x))
+#         count_list.append(count_city)
+#     count_list.sort()
+#     maxi = max(count_list)
+#     print(maxi)
+    
+#     for a in olkeler.keys():
+#         b=olkeler.get(a)
+#         if int(len(b)) == maxi:
+#             print(a)
+    
+            
+             
+# CityCountMax()
+  # ek cox sehere sahib olan olkeni gostersin
+  
+
+
+
+
+
+
+# def CountAllCities():
+#     allcities = 0
+#     for i in olkeler:
+#         a = olkeler.get(i)
+#         count_city = len(a)
+#         allcities += count_city
+#     print(allcities)
+# CountAllCities()
+  # butun seherlerin sayini ekranda gostersin
+  
